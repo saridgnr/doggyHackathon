06bc2f0076e4f0f960c91f69137bbb4f2d39a46a
saridgnr <saridgnr@gmail.com>
2019-04-05 07:06:53 +0300

    sliders

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatButtonModule, MatCheckboxModule, MatIconModule, MatInput, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatIconModule, MatInput, MatInputModule, MatSliderModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MainComponent } from './main/main.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -49,7 +49,8 @@ import { DialogreviewComponent } from './dialogreview/dialogreview.component';
     MatGridListModule,
     MatTabsModule,
     MatDialogModule,
-    MatInputModule
+    MatInputModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- client/src/app/dialogreview/dialogreview.component.ts
+++ client/src/app/dialogreview/dialogreview.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialogreview.component.scss']
 })
 export class DialogreviewComponent implements OnInit {
+  cute:Number
+  annoying:Number
+  friendly:Number
+  sleepy:Number
+  silly:Number
+  grumpy:Number
+  noisy:Number
+  smelly:Number
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  submit(){
+    console.log(this.smelly)
+  }
 }
