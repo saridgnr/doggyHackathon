487467a35041835b0e539ced8add7f48068aab1a
shirelkd <shirelkd@gmail.com>
2019-04-05 03:51:49 +0300

    Profile 3

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatButtonModule, MatCheckboxModule, MatIconModule} from '@angular/material';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule, MatCheckboxModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MainComponent } from './main/main.component';
 import { ProfileComponent } from './profile/profile.component';
 import { WinnersComponent } from './winners/winners.component';
@@ -12,8 +12,11 @@ import { AlldogsComponent } from './alldogs/alldogs.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TabsComponent } from './tabs/tabs.component';
 import { MatCardModule } from '@angular/material/card';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { RatingsComponent } from './ratings/ratings.component';
+import { TrophiesComponent } from './trophies/trophies.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogvisitComponent } from './dialogvisit/dialogvisit.component';
 
 @NgModule({
   declarations: [
@@ -23,21 +26,25 @@ import { RatingsComponent } from './ratings/ratings.component';
     WinnersComponent,
     AlldogsComponent,
     TabsComponent,
-    RatingsComponent
+    RatingsComponent,
+    TrophiesComponent,
+    DialogvisitComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule, 
+    MatButtonModule,
     MatCheckboxModule,
     MatToolbarModule,
     MatIconModule,
     FlexLayoutModule,
     MatCardModule,
-    MatTabsModule
+    MatTabsModule,
+    MatDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [DialogvisitComponent]
 })
 export class AppModule { }
